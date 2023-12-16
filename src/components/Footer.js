bc4bff63d625eae0b24ad702ca0662f294a04e28
Valérian VERONA <valerian.verona@fivesgroup.com>
2023-12-16 17:05:25 +0100

    fix: footer address display

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ export default function Footer() {
             <Row className="gy-2">
                 <Col xs className="d-flex align-items-center justify-content-center">
                     <span class="material-icons pe-2">location_on</span>
-                    <a href="https://www.google.com/maps/place/Hypnose+Ericksonienne+-+Constant+Verona,+6+Av.+de+Creil,+60300+Senlis/@49.207406,2.576897,14z/data=!4m6!3m5!1s0x47e637a9047756af:0x1394a6565ef5904d!8m2!3d49.2074064!4d2.5768966!16s%2Fg%2F11kpdms6r2?hl=en&gl=FR" className="text-white link-underline link-underline-opacity-0 text-nowrap">6 avenue de Creil, RDC - Bâtiment B, Senlis 60300</a>
+                    <a href="https://www.google.com/maps/place/Hypnose+Ericksonienne+-+Constant+Verona,+6+Av.+de+Creil,+60300+Senlis/@49.207406,2.576897,14z/data=!4m6!3m5!1s0x47e637a9047756af:0x1394a6565ef5904d!8m2!3d49.2074064!4d2.5768966!16s%2Fg%2F11kpdms6r2?hl=en&gl=FR" className="text-white link-underline link-underline-opacity-0 text-nowrap">6 avenue de Creil, Senlis 60300</a>
                 </Col>
                 <Col xs className="d-flex align-items-center justify-content-center">
                     <span class="material-icons pe-2">mail</span>
