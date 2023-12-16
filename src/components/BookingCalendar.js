164a653d50f5a48bf5896184c7c92a37e5bd2967
Valérian VERONA <valerian.verona@fivesgroup.com>
2023-12-16 17:04:23 +0100

    fix(calendar): even weeks

--- src/components/BookingCalendar.js
+++ src/components/BookingCalendar.js
@@ -3,14 +3,21 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 export default function BookingCalendar () {
+    const isEvenWeek = (date) => {
+        const startOfYear = new Date(date.getFullYear(), 0, 1);
+        const millisecondsInWeek = 7 * 24 * 60 * 60 * 1000;
+        const weekNumber = Math.ceil(((date - startOfYear) / millisecondsInWeek) + 1);
+        return weekNumber % 2 === 0;
+    };
+
     const isBookable =(date) => {
         const today = new Date();
         const isPast = date < today;
 
         return !isPast && (date.getDay() === 2 ||
         date.getDay() === 4 ||
-        (date.getDay() === 5 && date.getDate() % 2 === 1) ||
-        (date.getDay() === 6 && date.getDate() % 2 === 1));
+        (date.getDay() === 5 && isEvenWeek(date)) ||
+        (date.getDay() === 6 && !isEvenWeek(date)));
     }
 
     const tileClassName = ({ date }) => {
