ba737c7a13b19ef39d657050afd5b82bc1e31a2f
Valérian VERONA <valerian.verona@fivesgroup.com>
2023-12-16 17:23:33 +0100

    fix(theming): nav color active

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
